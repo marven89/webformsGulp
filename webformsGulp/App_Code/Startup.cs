b4d4d10e832a3374e63b9ae4cde980ece55323df
Marwen khamassi <m.khamassi@applicam.com>
2019-06-25 14:12:24 +0100

    Add project files.

---
+++ webformsGulp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webformsGulp.Startup))]
+namespace webformsGulp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
